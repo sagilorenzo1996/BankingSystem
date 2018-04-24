5e85b1767b1e2850b2f1f4fe55f14c0a7f926dc8
unknown <sagilorenzo1996@mail.com>
2018-04-24 05:41:24 +0530

    sdsd

--- src/main/java/com/ABCBank/BankingSystem/repo/AccountRepository.java
+++ src/main/java/com/ABCBank/BankingSystem/repo/AccountRepository.java
@@ -1,12 +1,17 @@
 package com.ABCBank.BankingSystem.repo;
 
 import com.ABCBank.BankingSystem.domain.Account;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long>{
     Account findByNic(String nic);
     Account findByNicAndAccountType(String nic,String type);
     Account findByNicAndPassword(String nic,String password);
+
 }
--- src/main/java/com/ABCBank/BankingSystem/web/TransactionController.java
+++ src/main/java/com/ABCBank/BankingSystem/web/TransactionController.java
@@ -1,14 +1,20 @@
 package com.ABCBank.BankingSystem.web;
 
+import com.ABCBank.BankingSystem.domain.Account;
 import com.ABCBank.BankingSystem.domain.HandleAccount;
 import com.ABCBank.BankingSystem.repo.AccountRepository;
 import com.ABCBank.BankingSystem.repo.EmployeeRepository;
 import com.ABCBank.BankingSystem.repo.HandleAccountRepository;
 import com.ABCBank.BankingSystem.repo.ManageAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/action")
@@ -33,5 +39,10 @@ public class TransactionController {
     }
 
     @PostMapping("/withdraw/{accId}/{type}/{amount}/{empId}")
-    public String withdraw(@PathVariable Long accId)
+    public ResponseEntity<Object> withdraw(@PathVariable Long accId, @PathVariable String type, @PathVariable BigDecimal amount, @PathVariable Long empId)throws ResourceNotFoundException{
+
+
+
+        return ResponseEntity.status(HttpStatus.MULTI_STATUS).body("done "+accId+" "+type);
+    }
 }
