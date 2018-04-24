2f26b199a608a33dc60374297f15f6dad7788bbb
unknown <sagilorenzo1996@mail.com>
2018-04-24 16:06:55 +0530

    logins

---
+++ src/main/java/com/ABCBank/BankingSystem/web/LoginController.java
@@ -0,0 +1,58 @@
+package com.ABCBank.BankingSystem.web;
+
+import com.ABCBank.BankingSystem.domain.Account;
+import com.ABCBank.BankingSystem.domain.Employee;
+import com.ABCBank.BankingSystem.domain.Login;
+import com.ABCBank.BankingSystem.repo.AccountRepository;
+import com.ABCBank.BankingSystem.repo.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityExistsException;
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @PostMapping("/user")
+    public Account userLogin(@Valid @RequestBody Login login)throws ResourceNotFoundException{
+            Account account=accountRepository.findByNic(login.getUsername());
+            if (account==null){
+                throw new ResourceNotFoundException("account not found");
+            } else {
+                if (account.getPassword().equals(login.getPassword())) {
+                    return account;
+                } else {
+                    throw new ResourceNotFoundException("incorrect credentials");
+                }
+            }
+    }
+
+    @PostMapping("/employee")
+    public Employee employeeLogin(@Valid @RequestBody Login login)throws ResourceNotFoundException{
+        Long empId=Long.parseLong(login.getUsername());
+        Optional<Employee> employee=employeeRepository.findById(empId);
+        if (employee.isPresent()==false){
+            throw new ResourceNotFoundException("employee not found");
+        } else {
+            if (employee.get().getPassword().equals(login.getPassword())) {
+                return employee.get();
+            } else {
+                throw new ResourceNotFoundException("incorrect credentials");
+            }
+        }
+    }
+
+}
